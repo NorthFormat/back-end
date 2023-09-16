95dcc82c399d28589cc0001d10b350ac2a4336fc
SamiyKlassniy <kishko.2003@list.ru>
2023-09-16 17:58:22 +0400

    first commit

---
+++ northformat/src/main/java/ru/bebriki/northformat/NorthformatApplication.java
@@ -0,0 +1,13 @@
+package ru.bebriki.northformat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthformatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthformatApplication.class, args);
+	}
+
+}
