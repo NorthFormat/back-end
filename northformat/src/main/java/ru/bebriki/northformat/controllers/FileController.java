250de1463588918527ac536c85fb6dc9772f12a5
SamiyKlassniy <kishko.2003@list.ru>
2023-09-19 11:43:01 +0400

    probably last commit)))

--- northformat/src/main/java/ru/bebriki/northformat/controllers/FileController.java
+++ northformat/src/main/java/ru/bebriki/northformat/controllers/FileController.java
@@ -29,19 +29,21 @@ public class FileController {
 
     private final FileRepository fileRepository;
 
+    @Transactional
     @PostMapping("/upload")
     public ResponseEntity<byte[]> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
-            File fileEntity = new File();
-            fileEntity.setFileName(file.getOriginalFilename());
-            fileEntity.setData(file.getBytes());
-            Clock clock = Clock.systemUTC();
-            LocalDateTime time = LocalDateTime.now(clock);
-            fileEntity.setDateCreation(time);
+
+            File fileEntity = File.builder().fileName(file.getOriginalFilename())
+                    .dateCreation(LocalDateTime.now(Clock.systemUTC()))
+                    .data(file.getBytes())
+                    .build();
+
             fileRepository.save(fileEntity);
+
             return readFile(fileEntity);
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new byte[]{});
         }
     }
 
@@ -86,65 +88,70 @@ public class FileController {
         );
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentDisposition(ContentDisposition.builder("attachment")
-                .filename(fileEntity.getFileName()).build());
+        headers.setContentDisposition(
+                ContentDisposition.builder("attachment")
+                .filename(fileEntity.getFileName())
+                .build()
+        );
 
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(new ByteArrayResource(fileEntity.getData()));
     }
+
     @GetMapping("/getAll")
     public List<File> downloadFile() throws FileNotFoundException {
 
         List<File> files = fileRepository.findAll();
-        for(File f:files){
+        for (File f : files) {
             System.out.println(f);
         }
         return files;
     }
+
     @Transactional
     @PostMapping("/overwriting/{id}")
-    public ResponseEntity<String> overwriting(@RequestParam("text") String text, @PathVariable Long id) throws FileNotFoundException{
-        try{
+    public ResponseEntity<String> overwriting(@RequestParam("text") String text, @PathVariable Long id) throws FileNotFoundException {
+        try {
             File fileEntity = fileRepository.findById(id).orElseThrow(
-                    ()-> new FileNotFoundException("There is no file with id:" + id)
+                    () -> new FileNotFoundException("There is no file with id:" + id)
             );
             byte[] newText = text.getBytes();
             fileEntity.setData(newText);
             fileRepository.save(fileEntity);
             return ResponseEntity.ok("File overwriting successfully");
-        }catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to overwriting file");
         }
     }
 
     @Transactional
     @PutMapping("/work/{id}")
-    public ResponseEntity<byte[]> work(@RequestParam("text") String text, @PathVariable Long id) throws FileNotFoundException{
-            overwriting(text,id);
-            return readFile(id);
+    public ResponseEntity<byte[]> work(@RequestParam("text") String text, @PathVariable Long id) throws FileNotFoundException {
+        overwriting(text, id);
+        return readFile(id);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> delete( @PathVariable Long id) throws FileNotFoundException{
+    public ResponseEntity<String> delete(@PathVariable Long id) throws FileNotFoundException {
         try {
             File fileEntity = fileRepository.findById(id).orElseThrow(
                     () -> new FileNotFoundException("There is no file with id:" + id)
             );
             fileRepository.delete(fileEntity);
             return ResponseEntity.ok("File delete successfully");
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete file");
         }
     }
+
     @Scheduled(cron = "0 0 * * * *")
     public void deleteFilesByTime() throws FileNotFoundException {
         Clock clock = Clock.systemUTC();
         LocalDateTime time = LocalDateTime.now(clock);
         List<File> files = fileRepository.findAll();
-        for(File f:files){
-            if(time.getHour()-f.getDateCreation().getHour()>=1) delete(f.getId());
+        for (File f : files) {
+            if (time.getHour() - f.getDateCreation().getHour() >= 1) delete(f.getId());
         }
-        System.out.println("PIPI");
     }
 }
